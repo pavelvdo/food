1ecd2220c39aea283f3101ff63797435e34560ee
Pavel <pavelvdo@gmail.com>
2022-01-23 16:46:10 +0300

    To 49-rest

--- js/script.js
+++ js/script.js
@@ -123,38 +123,159 @@ window.addEventListener('DOMContentLoaded', () => {
 
     btnModal.forEach((btn) => {
 
-        btn.addEventListener ('click', () => {
-            
-            modal.classList.add('show');
-            modal.classList.remove('hide');
-            //отключение прокрутки при модальном окне
-            document.body.style.overflow = 'hidden';
-        });
+        btn.addEventListener ('click', openModal);
     });
+    
+    function openModal() {
+        modal.classList.add('show');
+        modal.classList.remove('hide');
+        //отключение прокрутки при модальном окне
+        document.body.style.overflow = 'hidden';
+        clearInterval(modalTimerId);
+    }
+
+    function closeModal() {
+        modal.classList.add('hide');
+        modal.classList.remove('show');
+        document.body.style.overflow = '';
+    }
+
+    btnClose.addEventListener ('click', closeModal);
+            
+    //делаем закрытие окна при нажатии на подложку
+    modal.addEventListener('click', (e) => {
+        if (e.target === modal) {
+            closeModal();
+        }
+    });
+         
+    //закрытие на клавишу esc
+    document.addEventListener('keydown', (e) => {
+        //console.log(e.code);
+        if(e.code === "Escape" && modal.classList.contains('show')) {
+            closeModal();
+        }
+    });
+
+    //открытие модального окна через промежуток времени
+    // const modalTimerId = setTimeout(openModal, 3000);
+
+    function showModalByScroll() {
+        //window.pageYOffset - прокрученная часть
+        //document.documentElement.clientHeight - высота видимой части в данный момент!
+        //document.documentElement.scrollHeight - полная высота прокрутки
+        if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight -1) {
+            openModal();
+            window.removeEventListener('scroll', showModalByScroll);
+        }
+    }
+
+    window.addEventListener('scroll', showModalByScroll);
+
+    //Используем классы для карточек
+    //Это сделал сам!
+    // class MenuCard {
+    //     constructor(menuName, text, price) {
+    //         this.menuName = menuName;
+    //         this.text = text;
+    //         this.price = price;
+    //     }
+    // }
+
+    // const vegy = new MenuCard ('Меню "Фитнес"', 'Капуста Брюссельская - отвратительная - 500 гр.', 500),
+    //       elite = new MenuCard ('Меню “Премиум”', 'Микс: икра красная + икра черная - 100гр.', 5000),
+    //       post = new MenuCard ('Меню "Постное"', 'Щи безвкусные - 500 гр.', 300);
+
+    // menuEdit(vegy);
+    // menuEdit(elite);
+    // menuEdit(post);
+
+    // //Функция измениния карточек меню на странице
+    // function menuEdit(menuId) {
+    //     const menu = document.querySelectorAll('.menu__item');
         
-        function closeModal() {
-            modal.classList.add('hide');
-            modal.classList.remove('show');
-            document.body.style.overflow = '';
+    //     menu.forEach((item) => { 
+    //         const menuName = item.querySelector('.menu__item-subtitle'),
+    //               text = item.querySelector('.menu__item-descr'),
+    //               price = item.querySelector('.menu__item-total');
+            
+    //         if (menuName.textContent === menuId.menuName){
+    //             //console.log(menuName.textContent);
+    //             text.textContent = menuId.text;
+    //             price.innerHTML = `<span>${menuId.price}</span> грн/день`;
+    //         }
+    //     });
+    // }
+
+    //из уроков
+    class MenuCard {
+        constructor(src, alt, title, descr, price, parentSelector, ...classes) {
+            this.src = src;
+            this.alt = alt;
+            this.title = title;
+            this.descr = descr;
+            this.price = price;
+            this.classes = classes;
+            this.parent = document.querySelector(parentSelector);
+            this.transfer = 80;
+            this.changeToRUB();
         }
 
-        btnClose.addEventListener ('click', closeModal);
+        changeToRUB() {
+            this.price =  this.price * this.transfer;
+        }
+
+        render() {
+            const element = document.createElement('div');
             
-        //делаем закрытие окна при нажатии на подложку
-        modal.addEventListener('click', (e) => {
-            if (e.target === modal) {
-                closeModal();
+            if (this.classes.length === 0) {
+                element.classList.add('menu__item');
+            }else {
+                this.classes.forEach(className => element.classList.add(className));
             }
-        });
-         
-        //закрытие на клавишу esc
-        document.addEventListener('keydown', (e) => {
-            //console.log(e.code);
-            if(e.code === "Escape" && modal.classList.contains('show')) {
-                closeModal();
-            }
-        });
+            
+            element.innerHTML = `
+                <img src="${this.src}" alt="${this.alt}">
+                <h3 class="menu__item-subtitle">${this.title}</h3>
+                <div class="menu__item-descr">${this.descr}</div>
+                <div class="menu__item-divider"></div>
+                <div class="menu__item-price">
+                    <div class="menu__item-cost">Цена:</div>
+                    <div class="menu__item-total"><span>${this.price}</span> руб/день</div>
+                </div>
+                        `;
+            this.parent.append(element);
+        }
+    }
 
-    //console.log(modal);
-
+    
+    new MenuCard(
+        'img/tabs/hamburger.jpg',
+        'hamburger',
+        'Меню: "Гамбургер"',
+        "Супер большой гамбургер подается с картошкой фри и большим стаканом колы",
+        5,
+        '.menu .container',
+        'menu__item',
+        'big'
+    ).render();
+    new MenuCard(
+        'img/tabs/vegy.jpg',
+        'vegy',
+        'Меню: "Веган"',
+        "Супер большой лист салата подается с ничем и большим стаканом воды",
+        1,
+        '.menu .container',
+        'menu__item'
+    ).render();
+    new MenuCard(
+        'img/tabs/elite.jpg',
+        'elite',
+        'Меню: "Премиум"',
+        "В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!",
+        100,
+        '.menu .container',
+        'menu__item'
+    ).render();
+    
 });
